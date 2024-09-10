798a1c7e4a2d93b805e87c7b9e843f0944c54f47
Rafael Sabino <rcsabino@paschoalotto.com.br>
2024-09-10 16:47:14 -0300

    feat: aula 01 criando um filestream

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        string enderecoDoArquivo  = "contas.txt";
+        FileStream fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
+        byte[] buffer             = new byte[1024]; // 1KB
+
+        fluxoDoArquivo.Read(buffer, 0, 1024);
+
+        // public override int Read(byte[] array, int offset, int count);
     }
 }
