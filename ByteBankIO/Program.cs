576ee5711994e67ef1f29ab26d0ba16d24fc6bb2
Rafael Sabino <rcsabino@paschoalotto.com.br>
2024-09-11 14:26:21 -0300

    feat: StreamReader

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -1,9 +1,27 @@
-﻿using System.Text;
+﻿using System.Globalization;
+using System.Text;
 
 partial class Program
 {
     static void Main(string[] args)
     {
+        string enderecoDoArquivo = "contas.txt";
+
+        using (FileStream fluxoDeArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+        {
+            StreamReader leitor = new StreamReader(fluxoDeArquivo);
+
+            //string? linha = leitor.ReadLine(); recupera a primeira linha do arquivo
+            //string? texto = leitor.ReadToEnd(); recupera a arquivo todo
+            //int numero = leitor.Read(); primeiro byte do arquivo
+
+            while (!leitor.EndOfStream)
+            {
+                string? linha = leitor.ReadLine();
+                Console.WriteLine(linha);
+            }
+        }
+           
         Console.ReadLine();
     }
 }
