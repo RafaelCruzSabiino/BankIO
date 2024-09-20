3c973c5870e538cd26c028337ac087835c75ddd9
Rafael Sabino <rcsabino@paschoalotto.com.br>
2024-09-20 09:16:40 -0300

    feat: insere facilidade para trabalhar com arquivo pequenos

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -6,7 +6,19 @@ partial class Program
 {
     static void Main(string[] args)
     {
-        UsarStreamDeEntrada();
+        // *** PARA TRABALHAR COM AS ARQUIVOS PEQUENOS ***        
+        string[] linhas = File.ReadAllLines("contas.txt");
+        Console.WriteLine(linhas.Length);
+
+        //foreach (string linha in linhas)
+        //{
+        //    Console.WriteLine(linha);
+        //}
+
+        var bytesArquivo = File.ReadAllBytes("contas.txt");
+        Console.WriteLine($"Arquivo contas.txt possui {bytesArquivo.Length} bytes");
+
+        File.WriteAllText("escrevendoComAClasseFile.txt", "Testando File.WriteAllText");
 
         Console.WriteLine("Aplicação Finalizada!");
 
